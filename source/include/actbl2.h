8d404c5098939a20e757ac1b18aec7e3343d49a2
Robert Moore <Robert.Moore@intel.com>
2021-02-12 11:25:23 -0800

    ACPI 6.4: NFIT: add Location Cookie field

--- source/include/actbl2.h
+++ source/include/actbl2.h
@@ -1299,6 +1299,7 @@ typedef struct acpi_nfit_system_address
     UINT64                  Address;
     UINT64                  Length;
     UINT64                  MemoryMapping;
+    UINT64                  LocationCookie;     /* ACPI 6.4 */
 
 } ACPI_NFIT_SYSTEM_ADDRESS;
 
@@ -1306,6 +1307,7 @@ typedef struct acpi_nfit_system_address
 
 #define ACPI_NFIT_ADD_ONLINE_ONLY       (1)     /* 00: Add/Online Operation Only */
 #define ACPI_NFIT_PROXIMITY_VALID       (1<<1)  /* 01: Proximity Domain Valid */
+#define ACPI_NFIT_LOCATION_COOKIE_VALID (1<<2)  /* 02: SPA location cookie valid (ACPI 6.4) */
 
 /* Range Type GUIDs appear in the include/acuuid.h file */
 
